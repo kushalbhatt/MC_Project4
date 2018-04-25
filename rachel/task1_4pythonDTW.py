8f99004b95c158eb3a4530b5fa2ec60fd9632a02
Kushal.Bhatt@asu.edu <Kushal.Bhatt@asu.edu>
2018-04-25 14:18:53 -0700

    Modified task 1.10 ready for further use

--- rachel/task1_4pythonDTW.py
+++ rachel/task1_4pythonDTW.py
@@ -1,5 +1,5 @@
-#/usr/bin/python
-#!/usr/bin/python
+# /usr/bin/python
+# !/usr/bin/python
 
 import sys
 import csv
@@ -8,221 +8,229 @@ import math
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 
-#global variables
-datasubject=[]
-datareference=[]
-DTW_feature=[]
+# global variables
+datasubject = []
+datareference = []
+DTW_feature = []
+
 
 def main():
-	dataPath = "D:/ASL/feature/ScaleSpace_CSV/";
-	savePath = "D:/ASL/feature/DTW/";
-	Subject = "CSE572_G02";
-	Reference = "CSE572_G04";
+    dataPath = "D:/ASL/feature/ScaleSpace_CSV/";
+    savePath = "D:/ASL/feature/DTW/";
+    Subject = "CSE572_G02";
+    Reference = "CSE572_G04";
+
 
 def run_DTW_Generation(subjectinputfile, referenceinputfile):
-	numSensors=1
-	global datasubject
-	global datareference
-	global DTW_feature
-	
-	datasubject = CSV2Memory(subjectinputfile)
-	datareference = CSV2Memory(referenceinputfile)
-	DTW_feature = [ [ None for i in range(numSensors*27) ] for j in range(int(len(datareference)/numSensors)*int(len(datasubject)/numSensors)) ]
-	col=0
-	for iSensor in range(0,((len(datasubject)*int(len(datareference)/numSensors)))):#iterate through all subjects and references for compare
-			for iOctave in range(0,3):#3 octaves per sensor
-				for iScale in range(0,9):#9 values=4Dogs+5scale-space for each octave
-					#produce float array from each string SS or DoG saved in 3D data array
-					#subject SS/DoG
-					num=0
-					x=0
-					subject_one=[]
-					item=datasubject[iSensor%(len(datasubject))][iOctave][iScale]
-					if ',' in item:
-						x = np.fromstring(item, dtype=float, sep=',')
-						subject_one = x.astype(np.float)
-					else:
-						x = np.array(item)
-						x = np.reshape(x, (1,))
-						subject_one = x.astype(np.float)
-					#reference SS/DoG
-					num=0
-					x=0
-					reference_one=[]
-					item=datareference[(iSensor%numSensors)+(int(iSensor/len(datasubject))*numSensors)][iOctave][iScale]
-					if ',' in item:
-						x = np.fromstring(item, dtype=float, sep=',')
-						reference_one = x.astype(np.float)
-					else:
-						x = np.array(item)
-						x = np.reshape(x, (1,))
-						reference_one = x.astype(np.float)
-					norm_subject_one = MinAndMaxNorm(subject_one)
-					norm_reference_one = MinAndMaxNorm(reference_one)
-					
-						#one segment to another segment is one row
-						#row is segments
-						#column is SS or DoG
-					distance, path = fastdtw(norm_subject_one, norm_reference_one, dist=euclidean)
-					DTW_feature[int(iSensor/numSensors)][col]=distance
-					if(col==(numSensors*27-1)):
-						col=0
-					else:
-						col=col+1
-							
-	with open("DTW_matrix.csv","w+") as my_csv:
-		csvWriter = csv.writer(my_csv,delimiter=',')
-		csvWriter.writerows(DTW_feature)
+    numSensors = 1
+    global datasubject
+    global datareference
+    global DTW_feature
+
+    datasubject = CSV2Memory(subjectinputfile)
+    datareference = CSV2Memory(referenceinputfile)
+    DTW_feature = [[None for i in range(numSensors * 27)] for j in
+                   range(int(len(datareference) / numSensors) * int(len(datasubject) / numSensors))]
+    col = 0
+    for iSensor in range(0, ((len(datasubject) * int(
+                len(datareference) / numSensors)))):  # iterate through all subjects and references for compare
+        for iOctave in range(0, 3):  # 3 octaves per sensor
+            for iScale in range(0, 9):  # 9 values=4Dogs+5scale-space for each octave
+                # produce float array from each string SS or DoG saved in 3D data array
+                # subject SS/DoG
+                num = 0
+                x = 0
+                subject_one = []
+                item = datasubject[iSensor % (len(datasubject))][iOctave][iScale]
+                if ',' in item:
+                    x = np.fromstring(item, dtype=float, sep=',')
+                    subject_one = x.astype(np.float)
+                else:
+                    x = np.array(item)
+                    x = np.reshape(x, (1,))
+                    subject_one = x.astype(np.float)
+                # reference SS/DoG
+                num = 0
+                x = 0
+                reference_one = []
+                item = datareference[(iSensor % numSensors) + (int(iSensor / len(datasubject)) * numSensors)][iOctave][
+                    iScale]
+                if ',' in item:
+                    x = np.fromstring(item, dtype=float, sep=',')
+                    reference_one = x.astype(np.float)
+                else:
+                    x = np.array(item)
+                    x = np.reshape(x, (1,))
+                    reference_one = x.astype(np.float)
+
+                norm_subject_one = MinAndMaxNorm(subject_one)
+                norm_reference_one = MinAndMaxNorm(reference_one)
+
+                # one segment to another segment is one row
+                # row is segments
+                # column is SS or DoG
+                distance, path = fastdtw(norm_subject_one, norm_reference_one, dist=euclidean)
+                DTW_feature[int(iSensor / numSensors)][col] = distance
+                if (col == (numSensors * 27 - 1)):
+                    col = 0
+                else:
+                    col = col + 1
+
+    with open(subjectinputfile+"DTW_matrix.csv", "w+") as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=',')
+        csvWriter.writerows(DTW_feature)
+
 
 def CSV2Memory(filename):
-	
-	string=""
-	list=[]
-	
-	#open the file and determine the number of rows in the csv
-	sum=0
-	f = open(filename, 'r+')
-	for line in f:
-		cleanedLine = line.strip()
-		cleanedLine = str(cleanedLine).replace(',','')
-		cleanedLine = str(cleanedLine).replace(' ','')
-		if cleanedLine: # is not empty
-			sum=sum+1
-	row_count = sum
-	f.close()
+    string = ""
+    list = []
 
+    # open the file and determine the number of rows in the csv
+    sum = 0
+    f = open(filename, 'r+')
+    for line in f:
+        cleanedLine = line.strip()
+        cleanedLine = str(cleanedLine).replace(',', '')
+        cleanedLine = str(cleanedLine).replace(' ', '')
+        if cleanedLine:  # is not empty
+            sum = sum + 1
+    row_count = sum
+    f.close()
 
-	#create a 3D list named data[x][y][z] where x is the row (which represents the sensor),
-	#y is the octave, and z is either a DoG or SS
-	data = [[[0 for k in range(0,9)] for j in range(0,3)] for i in range(0,row_count)]
+    # create a 3D list named data[x][y][z] where x is the row (which represents the sensor),
+    # y is the octave, and z is either a DoG or SS
+    data = [[[0 for k in range(0, 9)] for j in range(0, 3)] for i in range(0, row_count)]
 
-	#open the same file in order to do computation
-	f = open(filename, 'r+')
-	reader = csv.reader(f,delimiter=',')
-	row=0
-	for octave in reader:
-		#format data
-		octave = str(octave).replace('[','')
-		octave = str(octave).replace(']','')
-		#print (octave)
-		for x in range(1,len(octave)):
-			if(octave[x-1]=="'" and octave[x]==","):
-				print
-			elif(octave[x]==" "):
-				print
-			elif(octave[x]=="'"):
-				list.append(string)
-				string=""
-			else:
-				string+=octave[x]
-		octave1list = []
-		octave2list = []
-		octave3list = []
-		x=0
-		while x<len(list):
-			if(list[x]==''):
-				del list[x]
-			else:
-				x=x+1
-		if(len(list)>0):
-			#append list of strings to octave list
-			for x in range(len(list)):
-				if(x<9):
-					octave1list.append(list[x])
-				elif(x>=9 and x<18):
-					octave2list.append(list[x])
-				else:
-					octave3list.append(list[x])
-			#convert octave lists into float lists
-			for d in range(0,len(octave1list)):
-				[float(i) for i in octave1list[d].split(',')]
-			for d in range(0,len(octave2list)):
-				[float(i) for i in octave2list[d].split(',')]
-			for d in range(0,len(octave3list)):
-				[float(i) for i in octave3list[d].split(',')]
-			#print octave values
-			'''
-				for d in range(0,len(octave1list)):
-				print (octave1list[d])
-				for d in range(0,len(octave2list)):
-				print (octave2list[d])
-				for d in range(0,len(octave3list)):
-				print (octave3list[d])
-				'''
-			#store the octave list values into the corresponding 3D array space
-			for y in range(0,3):
-				for z in range(0,9):
-					if(y==0):
-						data[row][y][z]=octave1list[z]#parser[x*26+y*8+z]
-					elif(y==1):
-						data[row][y][z]=octave2list[z]#parser[]
-					else:
-						data[row][y][z]=octave3list[z]#parser[x*26+y*8+z]
-			#get ready to do computation on the next row of the csv
-			row=row+1
-			#delete lists so that they can be used as temporaries in next iteration
-			del octave1list[:]
-			del octave2list[:]
-			del octave3list[:]
-			del list[:]
-		else:
-			break;
-	f.close()
-	return data
+    # open the same file in order to do computation
+    f = open(filename, 'r+')
+    reader = csv.reader(f, delimiter=',')
+    row = 0
+    for octave in reader:
+        # format data
+        octave = str(octave).replace('[', '')
+        octave = str(octave).replace(']', '')
+        # print (octave)
+        for x in range(1, len(octave)):
+            if (octave[x - 1] == "'" and octave[x] == ","):
+                print
+            elif (octave[x] == " "):
+                print
+            elif (octave[x] == "'"):
+                list.append(string)
+                string = ""
+            else:
+                string += octave[x]
+        octave1list = []
+        octave2list = []
+        octave3list = []
+        x = 0
+        while x < len(list):
+            if (list[x] == ''):
+                del list[x]
+            else:
+                x = x + 1
+        if (len(list) > 0):
+            # append list of strings to octave list
+            for x in range(len(list)):
+                if (x < 9):
+                    octave1list.append(list[x])
+                elif (x >= 9 and x < 18):
+                    octave2list.append(list[x])
+                else:
+                    octave3list.append(list[x])
+            # convert octave lists into float lists
+            for d in range(0, len(octave1list)):
+                [float(i) for i in octave1list[d].split(',')]
+            for d in range(0, len(octave2list)):
+                [float(i) for i in octave2list[d].split(',')]
+            for d in range(0, len(octave3list)):
+                [float(i) for i in octave3list[d].split(',')]
+            # print octave values
+            '''
+                for d in range(0,len(octave1list)):
+                print (octave1list[d])
+                for d in range(0,len(octave2list)):
+                print (octave2list[d])
+                for d in range(0,len(octave3list)):
+                print (octave3list[d])
+                '''
+            # store the octave list values into the corresponding 3D array space
+            for y in range(0, 3):
+                for z in range(0, 9):
+                    if (y == 0):
+                        data[row][y][z] = octave1list[z]  # parser[x*26+y*8+z]
+                    elif (y == 1):
+                        data[row][y][z] = octave2list[z]  # parser[]
+                    else:
+                        data[row][y][z] = octave3list[z]  # parser[x*26+y*8+z]
+            # get ready to do computation on the next row of the csv
+            row = row + 1
+            # delete lists so that they can be used as temporaries in next iteration
+            del octave1list[:]
+            del octave2list[:]
+            del octave3list[:]
+            del list[:]
+        else:
+            break;
+    f.close()
+    return data
+
 
 def MinAndMaxNorm(input):
-	output=[]
-	del output[:]
-	max_value = np.amax(input)
-	min_value = np.amin(input)
-	for i in range(0,input.size):
-		if(max_value==min_value):
-			output.append(0)
-		elif(math.isnan((input[i] - min_value) / (max_value - min_value))):
-			output.append(0)
-		else:
-			output.append((input[i] - min_value) / (max_value - min_value))
-	return output
+    output = []
+    del output[:]
+    max_value = np.amax(input)
+    min_value = np.amin(input)
+    for i in range(0, input.size):
+        if (max_value == min_value):
+            output.append(0)
+        elif (math.isnan((input[i] - min_value) / (max_value - min_value))):
+            output.append(0)
+        else:
+            output.append((input[i] - min_value) / (max_value - min_value))
+    return output
+
 
 def dtw_c(s, t):
-	d = 0
-	D = []
-	i=0
-	j=0
-	j1=0
-	j2=0
-	cost=0.0
-	temp=0.0
-	ns=len(s)
-	nt=len(t)
+    d = 0
+    D = []
+    i = 0
+    j = 0
+    j1 = 0
+    j2 = 0
+    cost = 0.0
+    temp = 0.0
+    ns = len(s)
+    nt = len(t)
 
-	# initialization
-	D = [[-1 for x in range(nt+1)] for y in range(ns+1)]
-	
-	D[0][0] = 0;
-	
-	# dynamic programming
-	for i in range(1,ns+1):
-		j1 = 1
-		j2 = nt
-		for j in range(j1,j2+1):
-			cost = math.sqrt((s[i - 1] - t[j - 1])*(s[i - 1] - t[j - 1]))
-			temp = D[i - 1][j]
-			if (D[i][j - 1] != -1):
-				if (temp == -1 or D[i][j - 1]<temp):
-					temp = D[i][j - 1]
-			if (D[i - 1][j - 1] != -1):
-				if (temp == -1 or D[i - 1][j - 1]<temp):
-					temp = D[i - 1][j - 1]
-			
-			D[i][j] = cost + temp
+    # initialization
+    D = [[-1 for x in range(nt + 1)] for y in range(ns + 1)]
 
-	d = D
-	return d;
+    D[0][0] = 0;
 
-run_DTW_Generation("test1.csv","test2.csv")
-#run_DTW_Generation("1491432155750.csv","1491433099844.csv")
-#print (len(DTW_feature))
+    # dynamic programming
+    for i in range(1, ns + 1):
+        j1 = 1
+        j2 = nt
+        for j in range(j1, j2 + 1):
+            cost = math.sqrt((s[i - 1] - t[j - 1]) * (s[i - 1] - t[j - 1]))
+            temp = D[i - 1][j]
+            if (D[i][j - 1] != -1):
+                if (temp == -1 or D[i][j - 1] < temp):
+                    temp = D[i][j - 1]
+            if (D[i - 1][j - 1] != -1):
+                if (temp == -1 or D[i - 1][j - 1] < temp):
+                    temp = D[i - 1][j - 1]
+
+            D[i][j] = cost + temp
+
+    d = D
+    return d;
+
+
+run_DTW_Generation("test1.csv", "test2.csv")
+# run_DTW_Generation("1491432155750.csv","1491433099844.csv")
+# print (len(DTW_feature))
 '''
 if __name__ == "__main__":
-	main()'''
+    main()'''
--- task1_10user-specificDnn.py
+++ task1_10user-specificDnn.py
@@ -1,10 +1,15 @@
 import numpy as np
 import csv
 import numpy as np
+
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.utils import plot_model
+
+from task1_4pythonDTW import run_DTW_Generation
+
 '''
+--Kushal--
 Input::		Most Similar User set--- (In our case give the name of file that has relevant scale-space data for that user)
 Output::	After creating and training the User_Specific DNNs for each Donor
 			'Other' Test user segments can be classified as eat/non-eat/uncertain  
@@ -15,46 +20,87 @@ Once the most similar donor set is obtained.
 	e.g. if most similar set has 3 donors.... 3 specific DNN networks will be created
 
 Training Data:    Donor x Donor    (Eat x Eat)  and (Eat x Non-Eat) DTW Feature Matrix  (COmputed Same as fig 17,16)
-					Note:: Only those Donors who appear in most simialar set are used (ignore others)  
+					Note:: Only those Donors who appear in most similar set are used (ignore others)  
 					
 Testing Data:     Test user   (Eat x 'Other') DTW Feature Matrix 
 '''
+
+'''For each simialr donor a separate DNN model will be created
+	    That trained model will be stored in DNN_models so that later can be used for testing data'''
+DNN_models = []
+
+
+
+def DNN_TESTING():
+    """
+    @Ashni
+    TODO:// Prepare testing data from the test user and find the output from Trained DNNS.
+        Make Sense out of those results to label unknown test user segments"""
+
+    return
+
+
+
 def compute_user_specific_DNN(similar_donors):
-	#to-be-initialized based on DTW-Feature_Matrix rows
-	data_train = []
-	labels_train = []
+	for donor in similar_donors:
+		model = create_user_specific_DNN_Model()
+		# to-be-initialized based on DTW-Feature_Matrix rows
+		data_train = []
+		labels_train = []
+		'''Right now for simplicity just taking two donors
+		   Code can be easily modified to run with any number of donors  
+		   SAMPLE_DATA_FOR_USERSPECIFIC_DNN folder has separated EAT and NOn-EAT segments for two donors
+		   Use that for the sample demo'''
 
-	#4 layers each with 256 neurons
-	model = Sequential() #linear stack of layers
-	model.add(Dense(256, input_dim=486, activation='relu'))#add layers to DNN
+        #TRAINING DTW MATRIX CREATION
+        other_donors = []
+        for d in similar_donors:
+            if d != donor:
+                other_donors.append(d)
+                #EAT-VS-EAT  (Mark them as True)
+                run_DTW_Generation(donor + "_EAT.csv", d+"_EAT.csv")
+
+                #EAT-VS-NONEAT (Mark Them as False)
+                '''TODO:// If someone could write the code for labelling this '''
+                run_DTW_Generation(donor + "_EAT.csv", d + "NONEAT.csv")
+
+
+        '''Finally Training Time  Read the Generated DTW File into training data and label data
+            Feed those into DNN for training
+            FileName would be:: [donor]DTW_matrix.csv'''
+
+        model.fit(data_train, labels_train, epochs=20, batch_size=64)
+        #save this model for testing lateron
+        DNN_models.append(model)
+
+	plot_model(model, to_file="user-specific-model.png", show_shapes=True)
+	# x: The input data, as a Numpy array (or list of Numpy arrays if the model has multiple outputs).
+	# batch_size: Integer. If unspecified, it will default to 32.
+	# verbose: Verbosity mode, 0 or 1.
+	# steps: Total number of steps (batches of samples) before declaring the prediction round finished. Ignored with the default value of None.
+	# evaluate(x_test, y_test, batch_size=128)
+	print "Returning from user-specific-dnn"
+
+
+def create_user_specific_DNN_Model():
+	# 4 layers each with 256 neurons
+	model = Sequential()  # linear stack of layers
+	model.add(Dense(256, input_dim=486, activation='relu'))  # add layers to DNN
 	model.add(Dropout(0.5))
-	#Dropout consists in randomly setting a fraction rate of input units to 0 at each update
-	#during training time, which helps prevent overfitting.
+	# Dropout consists in randomly setting a fraction rate of input units to 0 at each update
+	# during training time, which helps prevent overfitting.
 	model.add(Dense(256, activation='relu'))
-	#keras.layers.Dense(units, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
+	# keras.layers.Dense(units, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
 	model.add(Dropout(0.5))
 	model.add(Dense(256, activation='relu'))
 	model.add(Dropout(0.5))
 	model.add(Dense(256, activation='relu'))
 	model.add(Dropout(0.5))
 
-	model.add(Dense(1, activation='sigmoid')) #output layer
+	model.add(Dense(1, activation='sigmoid'))  # output layer
 
 	model.compile(loss='binary_crossentropy',
 				  optimizer='adam',
 				  metrics=['accuracy'])
+	return model
 
-	model.fit(x_train, y_train,
-			  epochs=20,
-			  batch_size=64)
-	#In general: Larger batch sizes result in faster progress in training, but don't always converge as fast. Smaller batch sizes train slower, but can converge faster. It's definitely problem dependent.
-	#In general, the models improve with more epochs of training, to a point. They'll start to plateau in accuracy as they converge. Try something like 50 and plot number of epochs (x axis) vs. accuracy (y axis). You'll see where it levels out.
-	#batch size defines number of samples that going to be propagated through the network.
-
-	plot_model(model,to_file="user-specific-model.png",show_shapes = True)
-	#x: The input data, as a Numpy array (or list of Numpy arrays if the model has multiple outputs).
-	#batch_size: Integer. If unspecified, it will default to 32.
-	#verbose: Verbosity mode, 0 or 1.
-	#steps: Total number of steps (batches of samples) before declaring the prediction round finished. Ignored with the default value of None.
-	#evaluate(x_test, y_test, batch_size=128)
-	print "Returning from user-specific-dnn"
