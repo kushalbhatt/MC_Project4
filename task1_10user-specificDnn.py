5c110e146d418ec4b395b250798455cdf50b9768
Rachel Dedinsky <rdedinsk@asu.edu>
2018-04-25 16:50:55 -0700

    ran kushal's code to produce model visual

--- task1_10user-specificDnn.py
+++ task1_10user-specificDnn.py
@@ -6,7 +6,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.utils import plot_model
 
-from task1_4pythonDTW import run_DTW_Generation
+#from task1_4pythonDTW import run_DTW_Generation
 
 '''
 --Kushal--
@@ -53,25 +53,25 @@ def compute_user_specific_DNN(similar_donors):
 		   Use that for the sample demo'''
 
         #TRAINING DTW MATRIX CREATION
-        other_donors = []
-        for d in similar_donors:
-            if d != donor:
-                other_donors.append(d)
+		other_donors = []
+		for d in similar_donors:
+			if d != donor:
+				other_donors.append(d)
                 #EAT-VS-EAT  (Mark them as True)
-                run_DTW_Generation(donor + "_EAT.csv", d+"_EAT.csv")
+				run_DTW_Generation(donor + "_EAT.csv", d+"_EAT.csv")
 
                 #EAT-VS-NONEAT (Mark Them as False)
-                '''TODO:// If someone could write the code for labelling this '''
-                run_DTW_Generation(donor + "_EAT.csv", d + "NONEAT.csv")
+				'''TODO:// If someone could write the code for labelling this '''
+				run_DTW_Generation(donor + "_EAT.csv", d + "NONEAT.csv")
 
 
-        '''Finally Training Time  Read the Generated DTW File into training data and label data
+		'''Finally Training Time  Read the Generated DTW File into training data and label data
             Feed those into DNN for training
             FileName would be:: [donor]DTW_matrix.csv'''
 
-        model.fit(data_train, labels_train, epochs=20, batch_size=64)
+		model.fit(data_train, labels_train, epochs=20, batch_size=64)
         #save this model for testing lateron
-        DNN_models.append(model)
+		DNN_models.append(model)
 
 	plot_model(model, to_file="user-specific-model.png", show_shapes=True)
 	# x: The input data, as a Numpy array (or list of Numpy arrays if the model has multiple outputs).
@@ -79,7 +79,7 @@ def compute_user_specific_DNN(similar_donors):
 	# verbose: Verbosity mode, 0 or 1.
 	# steps: Total number of steps (batches of samples) before declaring the prediction round finished. Ignored with the default value of None.
 	# evaluate(x_test, y_test, batch_size=128)
-	print "Returning from user-specific-dnn"
+	print ("Returning from user-specific-dnn")
 
 
 def create_user_specific_DNN_Model():
@@ -103,4 +103,6 @@ def create_user_specific_DNN_Model():
 				  optimizer='adam',
 				  metrics=['accuracy'])
 	return model
+model = create_user_specific_DNN_Model()
+plot_model(model,to_file="user-specific-model.png",show_shapes=True)
 
